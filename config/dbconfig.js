87eed5679aff53220e7e1256673e8a85bf022f7f
PatrickAsiedu <pattribette@gmail.com>
2025-11-26 19:51:16 +0000

    added connection pool

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,5 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
+const { Pool } = require('pg')
+require('dotenv').config()
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -8,24 +7,24 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
-    ssl: {
-    rejectUnauthorized: false
-  }
-});
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
-
-// const pool = new Pool({
-//   connectionString: process.env.DB_URL,
-//   ssl: { rejectUnauthorized: false }, // Needed for most cloud Postgres providers
-// })
+  max: 20, // Maximum number of clients in the pool
+  min: 2, // Minimum number of clients in the pool
+  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+  connectionTimeoutMillis: 5000, // Return an error after 5 seconds if connection not established
+  maxUses: 7500, // Close connection after 7500 uses (prevents memory leaks)
+})
 
 pool.connect((err, client, release) => {
   if (err) {
-    console.error('Error connecting to PostgreSQL:', err.stack);
-    return;
+    console.error('Error connecting to PostgreSQL:', err.stack)
+    return
   }
-  console.log('Connected to PostgreSQL database');
-  release();
-});
+  console.log('Connected to PostgreSQL database')
+  release()
+})
 
-module.exports = pool;
+module.exports = pool
